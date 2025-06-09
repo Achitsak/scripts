9b8e1182dc6509419b1543152853e6fecb12e208
Achitsak <81982587+Achitsak@users.noreply.github.com>
2025-06-09 21:36:48 +0700

    Update qwertyuiop.lua

--- qwertyuiop.lua
+++ qwertyuiop.lua
@@ -21,7 +21,7 @@ _G.MasterpConfigLog = {
     },
     ['Seed'] = {'Carrot'},
     ['Settings'] = {
-        ['NgrokHttp'] = 'https://8815-2403-6200-8814-30c9-7d4a-c610-cf15-644c.ngrok-free.app'
+        ['NgrokHttp'] = 'https://14ae-2403-6200-8814-30c9-7d4a-c610-cf15-644c.ngrok-free.app'
     }
 }
 
